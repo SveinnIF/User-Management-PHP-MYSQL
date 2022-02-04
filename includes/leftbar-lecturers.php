41b4c545a59f0223d52f87be9fad831177ba5333
StianMM <64170108+StianMM@users.noreply.github.com>
2022-02-04 18:54:16 +0100

    lecturers skal ikke ha de notificationsa

--- includes/leftbar-lecturers.php
+++ includes/leftbar-lecturers.php
@@ -4,8 +4,6 @@
 			<li class="ts-label">Main</li>
 			<li><a href="feedback-lecturers.php"><i class="fa fa-envelope"></i> &nbsp;Feedback</a>
 			</li>
-			<li><a href="notification-lecturers.php"><i class="fa fa-bell"></i> &nbsp;Notification<sup style="color:red">*</sup></a>
-			</li>
 			</ul>
 		</nav>
 
