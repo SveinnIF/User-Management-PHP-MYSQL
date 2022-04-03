d69a92aacfb3694e5d59961aef7d419634fb14da
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-03 13:57:10 +0200

    Input validation

--- register-students.php
+++ register-students.php
@@ -18,32 +18,113 @@ if(isset($_POST['submit']))
 $id=uniqid();
 $name=$_POST['name'];
 $email=$_POST['email'];
-// steg 2, et sikrere passord 
-$password=password_hash($_POST['password'], PASSWORD_DEFAULT);
+$password=$_POST['password'];
 $fieldofstudy=$_POST['fieldofstudy'];
 $class=$_POST['class'];
 $receiver='Admin';
 
-$sql ="CALL studentRegistrationInfo(:id, :name, :email, :password, :fieldofstudy, :class, '1')";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':id', $id, PDO::PARAM_STR);
-$query-> bindParam(':name', $name, PDO::PARAM_STR);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> bindParam(':fieldofstudy', $fieldofstudy, PDO::PARAM_STR);
-$query-> bindParam(':class', $class, PDO::PARAM_STR);
-$query->execute();
+// validation
+$valicheck='';
+$uppercase    = preg_match('@[A-Z ÆØÅ]@', $password);
+$lowercase    = preg_match('@[a-z æøå]@', $password);
+$number       = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
 
-if('success')
-{
-echo "<script type='text/javascript'>alert('Registration Successful!');</script>";
-echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
-}
-else 
-{
-$error="Something went wrong. Please try again";
-}
+	// name validation
+    	if (empty($name)) {
+		$nameResponse = array(
+		    "type" => "nameError",
+		    "message" => "Name is required"
+		);
+    	}    
+    	else if (!preg_match("/^[a-zA-Z-' æøåÆØÅ]*$/", $name)) {
+		$nameResponse = array(
+		    "type" => "nameError",
+		    "message" => "Invalid name"
+		); 
+    	} 
+	else if (preg_match("/^[a-zA-Z-' æøåÆØÅ]*$/", $name)) {
+		$valicheck="nm";
+	}
+           
+	// email validation		
+	if (empty($email)) {
+		$emailResponse = array(
+			"type" => "emailError",
+			"message" => "Email is required"
+		);
+	}
+	else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$emailResponse = array(
+			"type" => "emailError",
+			"message" => "Invalid email"
+		);
+	}
+	else if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$valicheck .= "em";
+	}
+	
+	// password validation
+	if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+		$pwdResponse = array(
+			"type" => "passwordError",
+			"message" => "Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character."
+		);
+	}
+	else if ($uppercase && $lowercase && $number && $specialChars && strlen($password) > 8){
+		$password=password_hash($_POST['password'], PASSWORD_DEFAULT);
+		$valicheck .= "pw";
+	}
+	
+	// field of study validation
+    	if (empty($fieldofstudy)) {
+		$fosResponse = array(
+		    "type" => "fosError",
+		    "message" => "Field of study is required"
+		);
+	}    
+	else if (!preg_match("/^[a-zA-Z-' æøåÆØÅ]*$/", $fieldofstudy)) {
+		$fosResponse = array(
+		    "type" => "fosError",
+		    "message" => "Invalid field of study"
+		); 
+	} 
+	else if (preg_match("/^[a-zA-Z-' æøåÆØÅ]*$/", $fieldofstudy)) {
+		$valicheck .="fos";
+	}
 
+	// class validation
+	if(isset($_REQUEST['class']) && $_REQUEST['class'] == "0") { 
+		$classResponse = array(
+		    "type" => "classError",
+		    "message" => "Class is required"
+		);
+    	}    
+    	else if(isset($_REQUEST['class']) &&  !in_array($_REQUEST['class'], ["19/20", "20/21", "21/22"], true)) {
+		$classResponse = array(
+		    "type" => "classError",
+		    "message" => "Invalid class"
+		); 
+    	} 
+	else if(isset($_REQUEST['class']) &&  in_array($_REQUEST['class'], ["19/20", "20/21", "21/22"], true)) {
+		$valicheck .="cls";
+	}
+	
+	// Sender informasjonen til databasen om alle validations er suksessfulle
+	if($valicheck == "nmempwfoscls") {
+		$sql ="CALL studentRegistrationInfo(:id, :name, :email, :password, :fieldofstudy, :class, '1')";
+		$query= $dbh -> prepare($sql);
+		$query-> bindParam(':id', $id, PDO::PARAM_STR);
+		$query-> bindParam(':name', $name, PDO::PARAM_STR);
+		$query-> bindParam(':email', $email, PDO::PARAM_STR);
+		$query-> bindParam(':password', $password, PDO::PARAM_STR);
+		$query-> bindParam(':fieldofstudy', $fieldofstudy, PDO::PARAM_STR);
+		$query-> bindParam(':class', $class, PDO::PARAM_STR);
+		$query->execute();
+		
+		echo "<script type='text/javascript'>alert('Registration Successful!');</script>";
+		echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+	} 
 }
 ?>
 
@@ -76,54 +157,90 @@ $error="Something went wrong. Please try again";
 				<div class="row">
 					<div class="col-md-12">
 						<h1 class="text-center text-bold mt-2x">Student Registration</h1>
-                        <div class="hr-dashed"></div>
+                        			<div class="hr-dashed"></div>
 						<div class="well row pt-2x pb-3x bk-light text-center">
-                         <form method="post" class="form-horizontal" enctype="multipart/form-data" name="regform" onSubmit="return validate();">
-                            <div class="form-group">
-                            <label class="col-sm-1 control-label">Name<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="name" class="form-control" required>
-                            </div>
-                            <label class="col-sm-1 control-label">Email<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="email" class="form-control" required>
-                            </div>
-                            </div>
+                         			 <form method="post" class="form-horizontal" enctype="multipart/form-data" name="regform">
+                            
+							<div class="form-group">
+								<label class="col-sm-1 control-label">Name<span style="color:red">*</span></label>
+                            				<div class="col-sm-5">
+								<input type="text" name="name" class="form-control" required>
+									<?php if(!empty($nameResponse)) { ?>
+									<div class="response <?php echo $nameResponse["type"]; ?>
+									">
+									<?php echo $nameResponse["message"]; ?>
+									</div>
+									<?php }?>
+                            				</div>
+                            
+								<label class="col-sm-1 control-label">Email<span style="color:red">*</span></label>
+                            				<div class="col-sm-5">
+								<input type="text" name="email" class="form-control" required>
+									<?php if(!empty($emailResponse)) { ?>
+									<div class="response <?php echo $emailResponse["type"]; ?>
+									">
+									<?php echo $emailResponse["message"]; ?>
+									</div>
+									<?php }?>
+							</div>
+							</div>
 
-                            <div class="form-group">
-                            <label class="col-sm-1 control-label">Password<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="password" name="password" class="form-control" id="password" required >
-                            </div>
+                            				<div class="form-group">
+								<label class="col-sm-1 control-label">Password<span style="color:red">*</span></label>
+                            				<div class="col-sm-5">
+								<input type="password" name="password" class="form-control" id="password"  >
+									<?php if(!empty($pwdResponse)) { ?>
+									<div class="response <?php echo $pwdResponse["type"]; ?>
+									">
+									<?php echo $pwdResponse["message"]; ?>
+									</div>
+									<?php }?>
+                            				</div>
 
-                            <label class="col-sm-1 control-label">Field of study<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <input type="text" name="fieldofstudy" class="form-control" required>
-                            </div>
-                            </div>
+								<label class="col-sm-1 control-label">Field of study<span style="color:red">*</span></label>
+                           			 	<div class="col-sm-5">
+								<input type="text" name="fieldofstudy" class="form-control" required>
+									<?php if(!empty($fosResponse)) { ?>
+									<div class="response <?php echo $fosResponse["type"]; ?>
+									">
+									<?php echo $fosResponse["message"]; ?>
+									</div>
+									<?php }?>
+							</div>
+							</div>
 							
 							<div class="form-group">
-                            <label class="col-sm-1 control-label">Class<span style="color:red">*</span></label>
-                            <div class="col-sm-5">
-                            <select name="class" class="form-control" required>
-                            <option value="">Select</option>
-                            <option value="19/20">19/20</option>
-                            <option value="20/21">20/21</option>
-							<option value="21/22">21/22</option>
-                            </select>
-                            </div>
+								<label class="col-sm-1 control-label">Class<span style="color:red">*</span></label>
+		    					<div class="col-sm-5">
+								<select name="class" class="form-control" required>
+									<option value="0">Select</option>
+									<option value="19/20">19/20</option>
+									<option value="20/21">20/21</option>
+									<option value="21/22">21/22</option>
+								</select>
+									<?php if(!empty($classResponse)) { ?>
+									<div class="response <?php echo $classResponse["type"]; ?>
+									">
+									<?php echo $classResponse["message"]; ?>
+									</div>
+									<?php }?>
+                            				</div>
 							</div>
 
 
-								<br>
-                                <button class="btn btn-primary" name="submit" type="submit">Register</button>
-                                </form>
-                                <br>
-                                <br>
-								<p>Already Have An Account? <a href="index.php" >Signin</a></p>
-								<p>Are You A Lecturer? <a href="register-lecturers.php" >Signup as lecturer</a></p>
-							</div>
+							<br>
+							
+				 			<button class="btn btn-primary" name="submit" type="submit">Register</button>
+							</form>
+							
+							<br>
+							<br>
+							
+							<p>Already Have An Account? <a href="index.php" >Signin</a></p>
+							<p>Are You A Lecturer? <a href="register-lecturers.php" >Signup as lecturer</a></p>
+							
 						</div>
+					</div>
 				</div>
 			</div>
 		</div>
