384b4c417163dff4cd524c2560dcce62b99bc589
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-07 13:17:04 +0200

    La til ett sekund delay

--- admin/index.php
+++ admin/index.php
@@ -15,22 +15,23 @@ $logger->pushHandler($handler);
 include('includes/config.php');
 if(isset($_POST['login']))
 {
-$email=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="CALL adminLoginCheck(:email, :password)";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
+	$email=$_POST['username'];
+	$password=md5($_POST['password']);
+	$sql ="CALL adminLoginCheck(:email, :password)";
+	$query= $dbh -> prepare($sql);
+	$query-> bindParam(':email', $email, PDO::PARAM_STR);
+	$query-> bindParam(':password', $password, PDO::PARAM_STR);
+	$query-> execute();
+	$results=$query->fetchAll(PDO::FETCH_OBJ);
+	
 if($query->rowCount() > 0)
 {
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+	sleep(1);
+	$_SESSION['alogin']=$_POST['username'];
+	echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
 } else{
-  
-  echo "<script>alert('Invalid Details');</script>";
-
+  	sleep(1);
+  	echo "<script>alert('Invalid Details');</script>";
 }
 
 }
