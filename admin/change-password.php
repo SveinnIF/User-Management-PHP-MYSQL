58d3dd6f6c38ae0c07b38e85e0ae83d9e6d381f8
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-07 13:14:56 +0200

    La til ett sekund delay

--- admin/change-password.php
+++ admin/change-password.php
@@ -21,27 +21,30 @@ header('location:index.php');
 else{
 // Code for change password	
 if(isset($_POST['submit']))
-	{
-$password=md5($_POST['password']);
-$newpassword=md5($_POST['newpassword']);
-$username=$_SESSION['alogin'];
-$sql ="SELECT Password FROM admin WHERE UserName=:username and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
+{
+	$password=md5($_POST['password']);
+	$newpassword=md5($_POST['newpassword']);
+	$username=$_SESSION['alogin'];
+	$sql ="SELECT Password FROM admin WHERE UserName=:username and Password=:password";
+	$query= $dbh -> prepare($sql);
+	$query-> bindParam(':username', $username, PDO::PARAM_STR);
+	$query-> bindParam(':password', $password, PDO::PARAM_STR);
+	$query-> execute();
+	$results = $query -> fetchAll(PDO::FETCH_OBJ);
+	
 if($query -> rowCount() > 0)
 {
-$con="CALL updatePwAdmin(:username, :newpassword)";
-$chngpwd1 = $dbh->prepare($con);
-$chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
-$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-$chngpwd1->execute();
-$msg="Your Password succesfully changed";
+	sleep(1);
+	$con="CALL updatePwAdmin(:username, :newpassword)";
+	$chngpwd1 = $dbh->prepare($con);
+	$chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
+	$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+	$chngpwd1->execute();
+	$msg="Your Password succesfully changed";
 }
 else {
-$error="Your current password is not valid.";	
+	sleep(1);
+	$error="Your current password is not valid.";	
 }
 }
 ?>
