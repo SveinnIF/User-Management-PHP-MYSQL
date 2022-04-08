0097914a8c4f71ad8312e6a74f000038b2571477
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-08 13:08:25 +0200

    adminConfig.php i include delen

--- register-students.php
+++ register-students.php
@@ -12,7 +12,7 @@ $handler = new GelfHandler($publisher,Logger::DEBUG);
 $logger->pushHandler($handler);
 
 error_reporting(0);
-include('includes/config.php');
+include('includes/adminConfig.php');
 if(isset($_POST['submit']))
 {
 $id=uniqid();
