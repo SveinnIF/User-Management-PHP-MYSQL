fab8fd5e942d0bc15cbf014698ab0b0892717a37
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-08 12:54:37 +0200

    adminconfig.php i include delen

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@ $handler = new GelfHandler($publisher,Logger::DEBUG);
 $logger->pushHandler($handler);
 
 error_reporting(0);
-include('includes/config.php');
+include('includes/adminconfig.php');
 if(isset($_POST['login']))
 {
 	$username=$_POST['username'];
