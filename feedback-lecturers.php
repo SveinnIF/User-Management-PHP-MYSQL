b1b0b8d1468f0b506172b2203f91099c8a228e7e
StianMM <64170108+StianMM@users.noreply.github.com>
2022-04-11 18:14:17 +0200

    lecturerConfig

--- feedback-lecturers.php
+++ feedback-lecturers.php
@@ -13,7 +13,7 @@ $handler = new GelfHandler($publisher,Logger::DEBUG);
 $logger->pushHandler($handler);
 
 error_reporting(0);
-include('includes/config.php');
+include('includes/lecturerConfig.php');
 if(strlen($_SESSION['alogin'])==0)
 	{	
 header('location:index.php');
